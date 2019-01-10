350600b78de84839b8df63be4ea6934ef05e25f8
brave1984 <huangzhaowei1984@gmail.com>
2019-01-10 10:08:40 +0800

    2019-1-10

--- push_gh-pages.sh
+++ push_gh-pages.sh
@@ -3,9 +3,9 @@ cd _site
 git config --global user.email $email
 git config --global user.name $name
 git init
-git checkout -b gh-pages
+git checkout -b master
 git remote add origin https://$token@github.com/$name/$name.github.io.git
 git add .
 git commit -m 'Travis CI #$(date +%Y-%m-%d/%T)'
-git push -f origin gh-pages
+git push -f origin master
 
