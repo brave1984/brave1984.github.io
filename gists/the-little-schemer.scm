fe85ef7ed464aa94906fa9808141a155e56c9790
brave1984 <huangzhaowei1984@gmail.com>
2019-01-10 22:43:41 +0800

    2019-1-10

--- gists/the-little-schemer.scm
+++ gists/the-little-schemer.scm
@@ -69,3 +69,84 @@
        (cons new (cdr lat))]
       [else (cons (car lat)
 		  (subst new old (cdr lat)))])))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      [(null? lat) '()]
+      [(or (eq? (car lat) o1)
+	   (eq? (car lat) o2))
+       (cons new (cdr lat))]
+      [else (cons (car lat)
+		  (subst2 new o1 o2 (cdr lat)))])))
+
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) a)
+       (multirember a (cdr lat))]
+      [else (cons (car lat)
+		  (multirember a (cdr lat)))])))
+
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old)
+       (cons old
+	     (cons new
+		   (multiinsertR new old (cdr lat))))]
+      [else (cons (car lat)
+		  (multiinsertR new old (cdr lat)))])))
+
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old)
+       (cons new
+	     (cons old
+		   (multiinsertL new old (cdr lat))))]
+      [else (cons (car lat)
+		  (multiinsertL new old (cdr lat)))])))
+
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old)
+       (cons new
+	     (multisubst new old (cdr lat)))]
+      [else (cons (car lat)
+		  (multisubst new old (cdr lat)))])))
+
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+
+(define o+
+  (lambda (n m)
+    (cond
+      [(zero? m) n]
+      [else (add1 (o+ n (sub1 m)))])))
+
+
+(define o-
+  (lambda (n m)
+    (cond
+      [(zero? m) n]
+      [else (sub1 (o- n (sub1 m)))])))
+
+(printf "~s\n" (o- 16 8))
