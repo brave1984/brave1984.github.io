a71a815babedb62e67269819621c78f3d59307e6
brave1984 <huangzhaowei1984@gmail.com>
2019-01-22 13:44:00 +0800

    2019-1-22

--- push_site.sh
+++ push_site.sh
@@ -5,6 +5,6 @@ git config --global user.name $name
 git init
 git remote add origin https://$token@github.com/$name/$name.github.io.git
 git add .
-git commit -m `Travis CI $(date +%Y-%m-%d/%T)`
+git commit -m "Travis CI $(date +%Y-%m-%d/%T)"
 git push -f origin master
 
